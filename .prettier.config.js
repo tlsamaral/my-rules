48b53151eb24e73cea4d421d07b066ff39b45fca
Talles Amaral <97260722+tlsamaral@users.noreply.github.com>
2024-07-29 16:08:38 -0300

    Create .prettier.config.js

---
+++ .prettier.config.js
@@ -0,0 +1,11 @@
+// prettier.config.js
+module.exports = {
+  semi: false, // Adiciona ponto e vírgula no final das declarações
+  singleQuote: true, // Usa aspas simples em vez de aspas duplas
+  trailingComma: "all", // Adiciona vírgula no final de objetos e arrays
+  arrowParens: "avoid", // Evita parênteses em funções de uma única linha
+  printWidth: 80, // Define a largura máxima da linha
+  tabWidth: 2, // Define o tamanho da indentação
+  bracketSpacing: true, // Adiciona espaçamento entre chaves e objetos
+  // jsxBracketSameLine: false, // Quebra a linha após o `>` nas tags JSX
+};
