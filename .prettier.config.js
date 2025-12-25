79f308dd5551c6e77cc9d4f8308e90139b0c4c4b
Talles Amaral <worktalles@hotmail.com>
2025-12-25 19:37:52 -0300

    Update .prettier.config.js

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,5 +1,6 @@
 // prettier.config.js
 module.exports = {
+  
   semi: false, // Adiciona ponto e vírgula no final das declarações
   singleQuote: true, // Usa aspas simples em vez de aspas duplas
   trailingComma: "all", // Adiciona vírgula no final de objetos e arrays
